bb614d17d824ebc0a6d21978b6cb69a15a44f4d2
Mark Taiwan <mark.taiwan@gmail.com>
2018-03-24 14:41:29 +0800

    More site changes

--- Ye Olde Derpibooru Uploader Description Layout.user.js
+++ Ye Olde Derpibooru Uploader Description Layout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Ye Olde Derpibooru Uploader Description Layout
 // @description Move uploader credit to its former location
-// @version     1.0.2
+// @version     1.0.3
 // @author      Marker
 // @license     MIT
 // @namespace   https://github.com/marktaiwan/
@@ -21,15 +21,18 @@
         imageDescription = document.querySelector('.image-description'),
         descriptionHeader = document.createElement('h3'),
         descriptionForm = document.querySelector('#description-form');
-  if (block === null)
+  // Exit if any of these elements doesn't exist
+  if ([block, imagemeta, extrameta, imageDescription].some(ele => ele === null)) {
     return;
+  }
   // Move uploader and image information
   extrameta.appendChild(document.querySelector('.image-size'));
 
   // Revert metadata bar
   imagemeta.classList.remove('image-metabar', 'flex--spaced-out');
-  extrameta.classList.remove('image-metabar', 'block__header', 'block__header--user-credit');
+  extrameta.classList.remove('image-metabar', 'flex--spaced-out', 'block__header', 'block__header--user-credit');
   extrameta.classList.add('block__header--sub', 'block__header--light');
+  extrameta.style.padding = '0px 12px'; // 0px up and down, 12px on the sides
 
   // Revert description box styling
   imageDescription.style.background = 'whitesmoke';

f47f29a495fdc771861263ec6536ddd1ee59899a
Mark Taiwan <mark.taiwan@gmail.com>
2018-04-05 11:37:34 +0800

    Site changes
    Derpibooru removed the forced metabar spacing and width, also moved the adbox back below the image.

--- Ye Olde Derpibooru Uploader Description Layout.user.js
+++ Ye Olde Derpibooru Uploader Description Layout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Ye Olde Derpibooru Uploader Description Layout
 // @description Move uploader credit to its former location
-// @version     1.0.4
+// @version     1.0.5
 // @author      Marker
 // @license     MIT
 // @namespace   https://github.com/marktaiwan/
@@ -21,19 +21,15 @@
         imageDescription = document.querySelector('.image-description'),
         descriptionForm = document.querySelector('#description-form'),
         content = document.querySelector('#content'),
-        tagBox = document.querySelector('.js-tagsauce');
+        tagBox = document.querySelector('.js-tagsauce'),
+        adBox = document.querySelector('#imagespns');
 
-  // Run if elements exists on page
-  if ([imagemeta, extrameta].every(ele => ele !== null)) {
-    // Move uploader and image information
-    extrameta.appendChild(document.querySelector('.image-size'));
-    // Revert metadata bar
-    imagemeta.classList.remove('image-metabar', 'flex--spaced-out');
-    extrameta.classList.remove('image-metabar', 'flex--spaced-out', 'block__header', 'block__header--user-credit');
-    extrameta.classList.add('block__header--sub', 'block__header--light');
-    extrameta.style.padding = '0px 12px'; // 0px up and down, 12px on the sides
+  // Revert metadata bar
+  if (extrameta !== null) {
+    extrameta.classList.add('block__header--light');
   }
 
+  // Run if elements exists on page
   if ([content, imageDescription, tagBox].every(ele => ele !== null)) {
     const descriptionHeader = document.createElement('h3'),
           newDiv = document.createElement('div');
@@ -43,6 +39,7 @@
     tagBox.querySelector('.js-imageform').classList.add('layout--narrow');
     content.insertBefore(newDiv, imageDescription.parentElement);
     content.insertBefore(tagBox, imageDescription.parentElement);
+    newDiv.appendChild(adBox);
     newDiv.appendChild(imageDescription);
     if (descriptionForm !== null) {
       newDiv.appendChild(descriptionForm);

56500528852874276e3c9ca2b88b9c9aa8161216
Mark Taiwan <mark.taiwan@gmail.com>
2019-05-17 12:05:04 +0800

    Version bump 1.0.15

--- Ye Olde Derpibooru Uploader Description Layout.user.js
+++ Ye Olde Derpibooru Uploader Description Layout.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Ye Olde Derpibooru Uploader Description Layout
 // @description Move uploader credit to its former location
-// @version     1.0.14
+// @version     1.0.15
 // @author      Marker
 // @license     MIT
 // @namespace   https://github.com/marktaiwan/
